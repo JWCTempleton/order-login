bb6366d43fb1645e4ac37392d5bf5d9efe1f6ce4
Jacob Templeton <jwt3@stmarys-ca.edu>
2021-03-04 14:04:34 -0800

    adding sign in form

--- order-login/src/App.js
+++ order-login/src/App.js
@@ -1,10 +1,12 @@
-import Navigation from './components/Navigation/Navigation'
+import Navigation from './components/Navigation/Navigation';
+import Signin from './components/Signin/Signin';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Navigation />
+      <Signin />
     </div>
   );
 }
